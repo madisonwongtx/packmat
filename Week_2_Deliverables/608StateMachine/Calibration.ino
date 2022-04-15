d0355867fc6da90cb83a7ea151f2dbc82f63d9bc
Michael Iglesias <michael@DHCP-POOL-18-25-27-083.MIT.EDU>
2022-04-15 16:17:20 -0400

    calibraton change

--- Week_2_Deliverables/608StateMachine/Calibration.ino
+++ Week_2_Deliverables/608StateMachine/Calibration.ino
@@ -1,61 +1,4 @@
-/*
-   -------------------------------------------------------------------------------------
-   HX711_ADC
-   Arduino library for HX711 24-Bit Analog-to-Digital Converter for Weight Scales
-   Olav Kallhovd sept2017
-   -------------------------------------------------------------------------------------
-*/
-
-/*
-   This example file shows how to calibrate the load cell and optionally store the calibration
-   value in EEPROM, and also how to change the value manually.
-   The result value can then later be included in your project sketch or fetched from EEPROM.
-
-   To implement calibration in your project sketch the simplified procedure is as follow:
-       LoadCell.tare();
-       //place known mass
-       LoadCell.refreshDataSet();
-       float newCalibrationValue = LoadCell.getNewCalibration(known_mass);
-*/
-
-#include <HX711_ADC.h>
-#if defined(ESP8266)|| defined(ESP32) || defined(AVR)
-#include <EEPROM.h>
-#endif
-
-//pins:
-const int HX711_dout = 8; //mcu > HX711 dout pin
-const int HX711_sck = 9; //mcu > HX711 sck pin
-
-//HX711 constructor:
-HX711_ADC LoadCell(HX711_dout, HX711_sck);
-
-const int calVal_eepromAdress = 0;
-unsigned long t = 0;
-
-void setup() {
-  Serial.begin(11520); delay(10);
-  Serial.println();
-  Serial.println("Starting...");
-
-  LoadCell.begin();
-  //LoadCell.setReverseOutput(); //uncomment to turn a negative output value to positive
-  unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
-  boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
-  LoadCell.start(stabilizingtime, _tare);
-  if (LoadCell.getTareTimeoutFlag() || LoadCell.getSignalTimeoutFlag()) {
-    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
-    while (1);
-  }
-  else {
-    LoadCell.setCalFactor(1.0); // user set calibration value (float), initial value 1.0 may be used for this sketch
-    Serial.println("Startup is complete");
-  }
-  while (!LoadCell.update());
-  calibrate(); //start calibration procedure
-}
-
-void loop() {
+float weight_readout(HX711_ADC LoadCell) {
   static boolean newDataReady = 0;
   const int serialPrintInterval = 0; //increase value to slow down serial print activity
 
@@ -85,8 +28,8 @@ void loop() {
   if (LoadCell.getTareStatus() == true) {
     Serial.println("Tare complete");
   }
-
 }
+
 
 void calibrate() {
   Serial.println("***");
