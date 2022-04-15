ca2ec21ebe7549b82d997b9bd63811f897828df0
Madison Wong <madiwong@mit.edu>
2022-04-15 15:55:47 -0400

    calibration set up in 608sm

--- Week_2_Deliverables/608StateMachine/608StateMachine.ino
+++ Week_2_Deliverables/608StateMachine/608StateMachine.ino
@@ -9,7 +9,21 @@ const int input4 = 37; //no, incorrect check, unlock mode
 uint8_t state;
 int update;
 
+//calibration set up
+#include <HX711_ADC.h>
+#if defined(ESP8266)|| defined(ESP32) || defined(AVR)
+#include <EEPROM.h>
+#endif
 
+//pins:
+const int HX711_dout = 8; //mcu > HX711 dout pin
+const int HX711_sck = 9; //mcu > HX711 sck pin
+
+//HX711 constructor:
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+
+const int calVal_eepromAdress = 0;
+unsigned long t = 0;
 
 //states 
 #define REST 0
@@ -42,6 +56,28 @@ int update;
 
 void setup() {
   // put your setup code here, to run once:
+  //calibration set up 
+  Serial.begin(11520); delay(10);
+  Serial.println();
+  Serial.println("Starting...");
+
+  LoadCell.begin();
+  //LoadCell.setReverseOutput(); //uncomment to turn a negative output value to positive
+  unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
+  boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
+  LoadCell.start(stabilizingtime, _tare);
+  if (LoadCell.getTareTimeoutFlag() || LoadCell.getSignalTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    while (1);
+  }
+  else {
+    LoadCell.setCalFactor(1.0); // user set calibration value (float), initial value 1.0 may be used for this sketch
+    Serial.println("Startup is complete");
+  }
+  while (!LoadCell.update());
+  calibrate(); //start calibration procedure
+
+  //button set up
   tft.init();
   tft.setRotation(2);
   tft.print("Welcome to PackMat");
